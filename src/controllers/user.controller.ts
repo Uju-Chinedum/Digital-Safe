c346f2a3a5e8a678f0421f092889449d996aa659
Uju-Chinedum <ujuchimaraoke@gmail.com>
2024-11-04 20:31:12 +0100

    feat(user): implemented update

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import UserModel from "../models/User";
 import { StatusCodes } from "http-status-codes";
+import { EditUserDto } from "../dto/editUser.dto";
+import { BadRequest, NotFound } from "../errors";
 
 export const getMe = async (req: Request, res: Response) => {
   const user = await UserModel.findOne({ _id: req.user?.userId });
@@ -15,7 +17,23 @@ export const getMe = async (req: Request, res: Response) => {
 };
 
 export const updateMe = async (req: Request, res: Response) => {
-  res.send("Update Me");
+  if (!req.body) {
+    throw new BadRequest("Missing Details", "Please provide fields for update");
+  }
+  const updateBody: EditUserDto = req.body;
+
+  const user = await UserModel.findByIdAndUpdate(req.user?.userId, updateBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(StatusCodes.OK).json({
+    status: StatusCodes.OK,
+    data: {
+      message: "User updated successfully",
+      user,
+    },
+  });
 };
 
 export const updatePassword = async (req: Request, res: Response) => {
---
+++ src/dto/editUser.dto.ts
@@ -0,0 +1,4 @@
+export interface EditUserDto {
+  name?: string;
+  email?: string;
+}
