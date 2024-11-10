78ca8536f96e0a85a662c7ccbc94895218b77c93
Uju Chinedum Chimaraoke <ujuchimaraoke@gmail.com>
2024-11-10 16:55:56 +0000

    feat: implemented getSafe

--- src/controllers/safe.controller.ts
+++ src/controllers/safe.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { BadRequest } from "../errors";
+import { BadRequest, NotFound, Unauthenticated } from "../errors";
 import { ISafe } from "../models";
 import SafeModel from "../models/Safe";
 import { StatusCodes } from "http-status-codes";
@@ -35,7 +35,37 @@ export const getSafes = async (req: Request, res: Response) => {
 };
 
 export const getSafe = async (req: Request, res: Response) => {
-  res.send("Get Safe");
+  const { id: safeId } = req.params;
+
+  const safe = (await SafeModel.findOne({
+    _id: safeId,
+    user: req.user?.userId,
+  })) as ISafe;
+  if (!safe) {
+    throw new NotFound("Safe Not Found", `No safe with id: ${safeId}`);
+  }
+
+  const password = req.query.password as string | undefined;
+  if (!password) {
+    throw new BadRequest(
+      "Missing Field",
+      `Please enter the password for ${safe.name}`,
+    );
+  }
+
+  const isPassword = await safe.comparePassword(password);
+  if (!isPassword) {
+    throw new Unauthenticated("Validation Error", "Invalid Password");
+  }
+
+  res.status(StatusCodes.OK).json({
+    status: "success",
+    data: {
+      code: StatusCodes.OK,
+      message: `Retrieved ${safe.name} successfully`,
+      safe,
+    },
+  });
 };
 
 export const updateSafe = async (req: Request, res: Response) => {
